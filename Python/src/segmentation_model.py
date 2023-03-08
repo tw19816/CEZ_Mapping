37ee727f2f02c3533c78b63ff1868241189b0e4c
tw19816 <tw19816@bristol.ac.uk>
2023-03-08 12:07:42 +0000

    Debugged segmentation model
    Replaced functionality in aspp.py with and custom implementation of an ASPP
    layer.

--- Python/src/segmentation_model.py
+++ Python/src/segmentation_model.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
-from aspp import ASPP
+from Python.src.aspp import ASPP
 
 def deeplabv3plus(
-    input_shape: tuple, batch_size: int, channels_low: int = 48
+    input_shape: tuple,
+    batch_size: int,
+    channels_low: int = 48,
+    channels_high: int = 2048,
+    middle_repeat: int = 16
 ) -> tf.keras.Model:
     """Instance of the DeepLabV3+ encoder-decoder architecture with a modified 
     Xception backbone.
@@ -12,9 +16,20 @@ def deeplabv3plus(
         https://arxiv.org/abs/1610.02357) (CVPR 2017)
     
     Args:
+        input_shape (tuple (int)) : Three integers used to specify the
+            dimensions of the input tensor in terms of x, y, channels 
+            respectively.
+        batch_size (int) : Number of inputs per batch.
+        channels_low (int) : Number of channels to down-sample low level
+            features to before combining with high-level features in decoder.
+        channels_high (int) : Number of channels to down-sample high level 
+            features to before combining with low-level features in decoder.
+            This is the number of channels from the Xception backbone.
+        middle_repeat (int) : Number of times to repeat middle Xception flow.
 
     Returns:
-        model (keras.Model) : A xception model instance.
+        model (keras.Model) : A DeepLabV3+ instance with an Xception backbone
+            and 256 output channels.
     """
     # Entry flow
     img_input = tf.keras.Input(shape=input_shape, batch_size=batch_size)
@@ -105,7 +120,7 @@ def deeplabv3plus(
     x = tf.keras.layers.add([x, residual])
 
     # Middle flow
-    for i in range(16):
+    for i in range(middle_repeat):
         residual = x
         prefix = "block" + str(i + 5)
 
@@ -151,49 +166,49 @@ def deeplabv3plus(
     )(x)
     residual = tf.keras.layers.BatchNormalization()(residual)
 
-    x = tf.keras.layers.Activation("relu", name="block13_sepconv1_act")(x)
+    x = tf.keras.layers.Activation("relu", name="block21_sepconv1_act")(x)
     x = tf.keras.layers.SeparableConv2D(
-        728, (3, 3), padding="same", use_bias=False, name="block13_sepconv1"
+        728, (3, 3), padding="same", use_bias=False, name="block21_sepconv1"
     )(x)
     x = tf.keras.layers.BatchNormalization(
-        name="block13_sepconv1_bn"
+        name="block21_sepconv1_bn"
     )(x)
-    x = tf.keras.layers.Activation("relu", name="block13_sepconv2_act")(x)
+    x = tf.keras.layers.Activation("relu", name="block21_sepconv2_act")(x)
     x = tf.keras.layers.SeparableConv2D(
-        1024, (3, 3), padding="same", use_bias=False, name="block13_sepconv2"
+        1024, (3, 3), padding="same", use_bias=False, name="block21_sepconv2"
     )(x)
-    x = tf.keras.layers.BatchNormalization(name="block13_sepconv2_bn")(x)
-    x = tf.keras.layers.Activation("relu", name="block13_sepconv3stride_act")(x)
+    x = tf.keras.layers.BatchNormalization(name="block21_sepconv2_bn")(x)
+    x = tf.keras.layers.Activation("relu", name="block21_sepconv3stride_act")(x)
     x = tf.keras.layers.SeparableConv2D(
         1024,
         (3, 3),
         strides=(2, 2),
         padding="same",
         use_bias=False,
-        name="block13_sepconv3stride"
+        name="block21_sepconv3stride"
     )(x)
-    x = tf.keras.layers.BatchNormalization(name="block13_sepconv3stride_bn")(x)
+    x = tf.keras.layers.BatchNormalization(name="block21_sepconv3stride_bn")(x)
 
     x = tf.keras.layers.add([x, residual])
 
     x = tf.keras.layers.SeparableConv2D(
-        1536, (3, 3), padding="same", use_bias=False, name="block14_sepconv1"
+        1536, (3, 3), padding="same", use_bias=False, name="block22_sepconv1"
     )(x)
-    x = tf.keras.layers.BatchNormalization(name="block14_sepconv1_bn")(x)
-    x = tf.keras.layers.Activation("relu", name="block14_sepconv1_act")(x)
+    x = tf.keras.layers.BatchNormalization(name="block22_sepconv1_bn")(x)
+    x = tf.keras.layers.Activation("relu", name="block22_sepconv1_act")(x)
     x = tf.keras.layers.SeparableConv2D(
-        1536, (3, 3), padding="same", use_bias=False, name="block14_sepconv2"
+        1536, (3, 3), padding="same", use_bias=False, name="block22_sepconv2"
     )(x)
-    x = tf.keras.layers.BatchNormalization(name="block14_sepconv2_bn")(x)
-    x = tf.keras.layers.Activation("relu", name="block14_sepconv2_act")(x)
+    x = tf.keras.layers.BatchNormalization(name="block22_sepconv2_bn")(x)
+    x = tf.keras.layers.Activation("relu", name="block22_sepconv2_act")(x)
 
     x = tf.keras.layers.SeparableConv2D(
-        2048, (3, 3), padding="same", use_bias=False, name="block14_sepconv3"
+        channels_high, (3, 3), padding="same", use_bias=False, name="block22_sepconv3"
     )(x)
     x = tf.keras.layers.BatchNormalization(
-        name="block14_sepconv3_bn"
+        name="block22_sepconv3_bn"
     )(x)
-    out_backbone = tf.keras.layers.Activation("relu", name="block14_sepconv3_act")(x)
+    out_backbone = tf.keras.layers.Activation("relu", name="block22_sepconv3_act")(x)
 
     # Decoder
     out_low = tf.keras.layers.Conv2D(
@@ -207,17 +222,28 @@ def deeplabv3plus(
     )(out_low)
 
     # Intermediate output dimensions 
-    tmp_target_h = tf.shape(out_low)[1]
-    tmp_target_w = tf.shape(out_low)[2]
+    tmp_target_h = out_low.get_shape().as_list()[1]
+    tmp_target_w = out_low.get_shape().as_list()[2]
+
 
     out_decode = ASPP(256, (6, 12, 18), name="dec_aspp")(out_backbone)
+    out_decode = tf.keras.layers.Conv2D(
+        256, (1, 1), use_bias=False, name="dec_conv1_high"
+    )(out_decode)
+    out_decode = tf.keras.layers.BatchNormalization(
+        name="dec_conv1_high_bn"
+    )(out_decode)
+    out_decode = tf.keras.layers.Activation(
+        "relu", name="dec_conv1_high_act"
+    )(out_decode)
+
     out_decode = tf.keras.layers.Resizing(
         tmp_target_h,
         tmp_target_w,
-        interpolation="biliear",
+        interpolation="bilinear",
         crop_to_aspect_ratio=False
     )(out_decode)
-
+    
     out_decode = tf.keras.layers.concatenate([out_decode, out_low])
 
     out_decode = tf.keras.layers.Conv2D(
@@ -232,8 +258,6 @@ def deeplabv3plus(
 
     out_decode = tf.keras.layers.Activation("relu", name="dec1_conv2_act")(out_decode)
 
-    # Create modified xception backbone
-    backbone = tf.keras.Model(img_input, out_backbone)
     # Create full model
     model = tf.keras.Model(img_input, out_decode)
     return model
