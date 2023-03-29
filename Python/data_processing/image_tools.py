6fe888d8fefd44659a18c2dc5d048221e162a0c8
tw19816 <tw19816@bristol.ac.uk>
2023-03-29 22:56:37 +0100

    Added functions and unit test to image_tools
    New functions:
    - save_image
    
    New unit tests:
    - load_image_dir_to_array

--- Python/data_processing/image_tools.py
+++ Python/data_processing/image_tools.py
@@ -22,6 +22,31 @@ def load_image(image_path: str) -> np.ndarray:
     return image
 
 
+def save_image(image: np.ndarray, path: str) -> None:
+    """Save an integer image array as a png image.
+    
+    Args:
+        image (np.ndarray (int)) : Image array to save, must contain 
+            integer pixel values.
+        path (str) : Path to save location, must end in .png.
+    
+    Errors:
+        FileExistsError : If path already exists.
+        ValueError : If image path does not end in .png
+        ValueError : If image dtype is not int
+    """
+    if os.path.exists(path):
+        raise FileExistsError(f"file already exists at {path}")
+    extension = path.split(".")[-1]
+    if extension != "png":
+        raise ValueError("path must end in .png")
+    if np.issubdtype(image.dtype, np.integer):
+        raise ValueError("image must be an interger array")
+    Image.fromarray(image).save(path)
+    
+
+
+
 def load_image_dir_to_array(
     image_dir_path: str, sorted: bool = True, normalised=False
 ) -> tuple[np.ndarray, list[str]]:
@@ -33,10 +58,11 @@ def load_image_dir_to_array(
         sorted (bool) : If true, sorts images based on filenames.
         
     Returns:
-        image_dataset (np.ndarray) : Array of images with dimensions: 
-            image number, pixel row, pixel column, RGB channel.
-        image_paths (list (str)) : List of image paths in the order which the
-            images are loaded into array.
+        image_dataset (np.ndarray (np.float32)) : Array of images with 
+            dimensions: image number, pixel row, pixel column, RGB 
+            channel.
+        image_paths (list (str)) : List of image paths in the order 
+            which the images are loaded into array.
     """
     img_lookup = os.path.join(image_dir_path, '*.png')
     image_paths = glob(img_lookup)
@@ -47,7 +73,6 @@ def load_image_dir_to_array(
     if normalised:
         image_dataset = image_dataset / 255.0
     return image_dataset, image_paths
-
 
 
 def create_greyscale_masks(
@@ -70,11 +95,15 @@ def create_greyscale_masks(
             index values to labels.
         
     Errors:
+        FileExistsError : If output_dir_path already exists but is not a
+            directory.
         FileExistsError: If an image already exists in the output 
             directory with the same name as an image in the input
             directory.
     """
-    image_dataset_rgb, image_paths = load_image_dir_to_array(image_dir_path, sorted=False)
+    image_dataset_rgb, image_paths = load_image_dir_to_array(
+        image_dir_path, sorted=False
+    )
     image_dataset_rgb = np.rint(image_dataset_rgb).astype(np.uint8)
     image_dataset_greyscale = dataset_rgb_to_index(
         image_dataset_rgb,
@@ -83,12 +112,14 @@ def create_greyscale_masks(
     )
     image_dir = os.path.split(image_paths[0])[0]
     image_dir_greyscale = image_dir + "_greyscale"
-    if not os.path.isdir(image_dir_greyscale):
-        os.mkdir(image_dir_greyscale)
+    if os.path.exists(image_dataset_greyscale):
+        if not os.path.isdir(image_dir_greyscale):
+            raise FileExistsError(
+                "output_dir_path already exists by it is not a directory"
+            )
     else:
-        raise FileExistsError(
-            f"Cannot make greyscale directory since it already exists: {image_dir_greyscale}"
-        )
+        os.mkdir(image_dir_greyscale)
+    
     for id, path in enumerate(image_paths):
         dir_path, filename = os.path.split(path)
         image_path_greyscale = os.path.join(image_dir_greyscale, filename)
